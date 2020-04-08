7b0863de74ef32ed1ef0dc7d76a412055d460518
Vincent Renault <vincent.renault7@gmail.com>
2020-04-08 12:32:24 +0200

    Update output path for dispatch chronics

--- chronix2grid/kpi/preprocessing/pivot_utils.py
+++ chronix2grid/kpi/preprocessing/pivot_utils.py
@@ -149,7 +149,7 @@ def chronics_to_kpi(year, n_scenario, repo_in, timestep, params, thermal = True,
 
         else:
             print('Year ' + str(year))
-            folder = os.path.join(repo_in, str(year), 'Scenario_' + str(n_scenario))
+            folder = os.path.join(repo_in, str(year), 'Scenario_' + str(n_scenario), 'chronics')
             prod_p = pd.read_csv(os.path.join(folder, 'prod_p.csv.bz2'), sep=';', decimal='.')
             load_p = pd.read_csv(os.path.join(folder, 'load_p.csv.bz2'), sep=';', decimal='.')
             price = pd.read_csv(os.path.join(folder, 'price.csv.bz2'), sep=';', decimal='.')
