d66516772ea56865b7279aa1cb4c6e445e70db25
NMegel <nico.megel@gmail.com>
2021-03-17 18:42:50 +0100

    fixed bug in output_processor

--- chronix2grid/output_processor.py
+++ chronix2grid/output_processor.py
@@ -15,8 +15,11 @@ def write_start_dates_for_chunks(output_path, scenario_name, n_weeks, by_n_weeks
     n_chunks = compute_n_chunks(n_weeks, by_n_weeks)
     start_date_time = pd.to_datetime(start_date, format='%Y-%m-%d')
     file_name = 'start_datetime.info'
-    
-    time_step = pd.to_datetime(f'00:{str(time_step)}', format='%H:%M')
+
+    if time_step == 60:
+        time_step = pd.to_datetime(f'01:00', format='%H:%M')
+    else:
+        time_step = pd.to_datetime(f'00:{str(time_step)}', format='%H:%M')
     file_name_ts = cst.TIME_STEP_FILE_NAME
     
     scen_name_generator = gu.folder_name_pattern(scenario_name, n_scenarios)
