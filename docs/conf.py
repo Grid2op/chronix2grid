3fc359abbb929763f111b56124d2d816d7dfe790
DONNOT Benjamin <benjamin.donnot@rte-france.com>
2022-05-31 13:58:49 +0200

    version 1.1.0.post1 => add some files for missing data

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Antoine Marot, Mario Jothy, Nicolas Megel'
 author = 'Antoine Marot, Mario Jothy, Nicolas Megel'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.0'
+release = '1.1.0.post1'
 version = '1.1'
 
 
