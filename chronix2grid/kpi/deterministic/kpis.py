8bdc16aace4370edb4aca94f1a68048d4409cd7f
BDonnot <benjamin.donnot@gmail.com>
2023-04-04 17:33:48 +0200

    fix a bug with old version of seaborn

--- chronix2grid/kpi/deterministic/kpis.py
+++ chronix2grid/kpi/deterministic/kpis.py
@@ -146,8 +146,8 @@ class EconomicDispatchValidator:
             df_syn = df_syn / df_syn.max()
 
         fig, axes = plt.subplots(1, 2, figsize=(17, 5))
-        sns.barplot(df_ref.index, df_ref, ax=axes[0])
-        sns.barplot(df_syn.index, df_syn, ax=axes[1])
+        sns.barplot(x=df_ref.index, y=df_ref, ax=axes[0])
+        sns.barplot(x=df_syn.index, y=df_syn, ax=axes[1])
         axes[0].set_title('Reference '+title_component, size = 9)
         axes[1].set_title('Synthetic '+title_component, size = 9)
 
@@ -1176,8 +1176,8 @@ class EconomicDispatchValidator:
         ## Plot results
         # By day
         fig, axes = plt.subplots(1, 2, figsize=(17, 5))
-        sns.barplot(conso_day_ref.index, conso_day_ref, ax=axes[0])
-        sns.barplot(conso_day.index, conso_day, ax=axes[1])
+        sns.barplot(x=conso_day_ref.index, y=conso_day_ref, ax=axes[0])
+        sns.barplot(x=conso_day.index, y=conso_day, ax=axes[1])
         axes[0].set_title('Reference - Normalized load per day of week', size=9)
         axes[1].set_title('Synthetic - Normalized load per day of week', size=9)
 
@@ -1188,8 +1188,8 @@ class EconomicDispatchValidator:
         every_nth = 3
 
         fig, axes = plt.subplots(1, 2, figsize=(17, 5))
-        sns.barplot(conso_week_ref.index, conso_week_ref, ax=axes[0])
-        sns.barplot(conso_week.index, conso_week, ax=axes[1])
+        sns.barplot(x=conso_week_ref.index, y=conso_week_ref, ax=axes[0])
+        sns.barplot(x=conso_week.index, y=conso_week, ax=axes[1])
         axes[0].set_title('Normalized load per week of year', size=9)
         for n, label in enumerate(axes[0].xaxis.get_ticklabels()):
             if n % every_nth != 0:
